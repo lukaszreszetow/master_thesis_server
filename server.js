6c0e8c6b11782149b046eb8d00ff6948493215e1
Łukasz Reszetow <lukasz.reszetow@birghtinventions.pl>
2019-06-23 11:49:46 +0200

    sound and pdf added

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ io.sockets.on('connection', function (socket) {
     console.log("Someone just connected!");
 
     // Echo back messages from the client
-    socket.on('message', function (message) {
-        console.log("Got message: " + message);
-        socket.emit('message', message);
+    socket.on('image', function (message) {
+        console.log("Got image of length: " + message.length);
+        socket.emit('image', message);
+    });
+
+    socket.on('sound', function (message) {
+        console.log("Got sound of length: " + message.length);
+        socket.emit('sound', message);
+    });
+
+    socket.on('pdf', function (message) {
+        console.log("Got pdf of length: " + message.length);
+        socket.emit('pdf', message);
     });
 });
